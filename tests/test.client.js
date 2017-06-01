c0d526d41e2cbbaf5c25e330c9f759ad75e85ccb
hannahbeale <hannahbeale@users.noreply.github.com>
2017-06-02 10:04:09 +1200

    Update and rename changemyname.js to test.client.js

---
+++ tests/test.client.js
@@ -0,0 +1,18 @@
+import test from 'ava'
+import nock from 'nock'
+
+import * as actions from '../../components'
+
+test.cb('fetchPosts', t => {
+  const scope = nock('http://localhost:80')
+    .get('/api/emotions/')
+    .reply(200, [{data: 'This works'}])
+
+  actions.fetchPosts('Happiness')((actual) => {
+    scope.done()
+    t.is(actual.type, 'RECEIVE_EMOTIONS')
+    t.is(actual.posts.length, 1)
+    t.is(actual.posts[0], 'This works too')
+    t.end()
+  })
+})
